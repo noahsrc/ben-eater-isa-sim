137b53a7599e93ddfa3a4b40f1a5f0a4f6e2a428
Noah Miller <77933519+noahsrc@users.noreply.github.com>
2021-05-15 17:48:32 -0400

    Bug fixes.
    Fixed crash while assembling without specifying a register number or immediate.
    Fixed crash while assembling without specifying a register number or immediate and including a comment.
    Fixed issue with stopping program execution.

---
+++ Assembler.cs
@@ -0,0 +1,184 @@
+﻿/*
+ * Written by Noah Miller
+ * 15 May 2021
+ * 
+ * My GitHub: https://github.com/noahsrc
+ * My YouTube: https://www.youtube.com/c/noahsrc
+ * 
+ * Ben Eater's GitHub: https://github.com/beneater
+ * Ben Eater's Youtube: https://www.youtube.com/user/eaterbc
+ * 
+ * This class assembles a program from
+ * assembler to hexidecimal using Ben
+ * Eater's 8-bit instruction set
+ * specifications.
+ * 
+ * A program is sent to the Assemble method
+ * and is parsed by each line. Returning true
+ * means the program was successfully assembled.
+ * Returning false, the opposite. Error codes
+ * are posted to the string "asmInfo". If
+ * returning false, the calling class will read
+ * the error information using method GetAsmInfo.
+ * 
+ * Instruction syntax:
+ * lda 3
+ * LDA 3
+ * lDa 3 this is a comment
+ * 
+ * Syntax cont.:
+ * Blank lines are allowed.
+ * Blank lines with spaces are allowed.
+ * To comment on a non-executing line, use ;
+ * as the first character.
+ */
+
+using System;
+using System.Linq;
+
+namespace eaterIsaSim
+{
+    public static class Assembler
+    {
+        // Error code if applicable
+        private static string asmInfo;
+
+        // Program data
+        private static uint[] pgm;
+
+        // Cursor location within program
+        private static int count = 0;
+
+        // Converts a line from assembler to
+        // its hexadecimal value
+        private static uint Parse(string line)
+        {
+            // Check for blank line or comment
+            if (String.IsNullOrWhiteSpace(line) || line.StartsWith(";"))
+            {
+                // Code for blank line or comment
+                return 0x0100;
+            }
+
+            // Tokenize the assembler line
+            string[] tokens = line.Split(' ');
+
+            // Initialize byte data
+            uint byteData = 0x00;
+
+            // Define opcode
+            uint opCode;
+
+            // Get opcode from assembler mnemonic
+            switch (tokens[0].ToLower())
+            {
+                case "nop": opCode = 0x0; break;
+                case "lda": opCode = 0x1; break;
+                case "add": opCode = 0x2; break;
+                case "sub": opCode = 0x3; break;
+                case "sta": opCode = 0x4; break;
+                case "ldi": opCode = 0x5; break;
+                case "jmp": opCode = 0x6; break;
+                case "jc":  opCode = 0x7; break;
+                case "jz":  opCode = 0x8; break;
+                case "out": opCode = 0xE; break;
+                case "hlt": opCode = 0xF; break;
+                // Error code for mnemonic not found
+                default: return 0x0200;
+            }
+
+            // Check if second token exists
+            if (tokens.Length == 1 || String.IsNullOrWhiteSpace(tokens[1]) || !tokens[1].All(char.IsDigit))
+            {
+                // Error code for register number or immediate not found
+                return 0x0300;
+            }
+
+            // Check if register number or immediate is within 4 bits
+            if (UInt32.Parse(tokens[1]) > 0xF)
+            {
+                // Error code for exceeding 4 bits
+                return 0x0400;
+            }
+
+            // Returns the combination of opcode and register number or immediate
+            // High nibble + low nibble = byte
+            // opCode is shifted into high nibble of byteData, register number or
+            // immediate is added to byteData
+            return Hex.SetHighNibble(byteData, opCode) + UInt32.Parse(tokens[1]);
+        }
+
+        // Assembles an assembler program
+        // Returns true on success, false
+        // if not.
+        public static bool Assemble(string[] data, uint MAX_BYTES)
+        {
+            count = 0;
+
+            // Allocate new program
+            pgm = new uint[MAX_BYTES];
+
+            // Define parsed line
+            uint parsedLine;
+
+            // Iterate through program data and parse each line
+            for (int i = 0; i < data.Length; i++)
+            {
+                // Convert line from assembler to hexidecimal
+                parsedLine = Parse(data[i]);
+
+                // Add line to program or post
+                // error code and return
+                switch(parsedLine)
+                {
+                    case 0x0100: // Null or whitespace
+                        break;
+                    case 0x0200: // Invalid opcode
+                        asmInfo = "Invalid opcode found at line " + (i + 1) + ".";
+                        return false;
+                    case 0x0300: // No number specified
+                        asmInfo = "Register number or immediate not specified at line " + (i + 1) + ".";
+                        return false;
+                    case 0x0400: // Number out of range
+                         asmInfo = "Number out of range at line " + (i + 1) + ". Must be a value 0 to 15.";
+                        return false;
+                    default:
+                        pgm[count] = parsedLine;
+                        count++;
+                        break;
+                }
+
+                // Check if program size meets or exceeds maxmum size
+                if (count >= MAX_BYTES)
+                {
+                    asmInfo ="Maximum byte count met. Last instruction loaded at line " + (i + 1) + ".";
+                    return false;
+                }
+            }
+
+            // Successfully assemble
+            return true;
+        }
+
+        // Returns the assebmled program
+        // Called only when Assemble method
+        // returns true
+        public static uint[] GetProgram()
+        {
+            return pgm;
+        }
+
+        // Returns error code posted by
+        // assembler
+        public static string GetAsmInfo()
+        {
+            return asmInfo;
+        }
+
+        // Returns number of bytes used by program
+        public static int GetBytesUsed()
+        {
+            return count;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿/*
+ * Written by Noah Miller
+ * 14 May 2021
+ * 
+ * My GitHub: https://github.com/noahsrc
+ * My YouTube: https://www.youtube.com/c/noahsrc
+ * 
+ * Ben Eater's GitHub: https://github.com/beneater
+ * Ben Eater's Youtube: https://www.youtube.com/user/eaterbc
+ * 
+ * This class creates the simulated memory
+ * and CPU to run programs on. The CPU uses
+ * Ben Eater's instruction set used in his
+ * 8-bit breadboard computer seen on YouTube.
+ * A sample fibonacci program is typed in
+ * the program workspace. Program information
+ * may be seen in the program output textox at
+ * the bottom of the form.
+ * 
+ * "Constant" variables may be set in the
+ * form textboxes on the right.
+ */
+
+//
+// Note: This form application is not yet
+//       complete. The finished version
+//       will transition the information
+//       from the debug console into a
+//       graphical Windows form.
+//
+
+/*
+ * Update (15 May 2021):
+ * 
+ * Began work on the graphical form window.
+ * -- Added workspace to create programs
+ * -- Added program output display
+ * -- Added output register display
+ * -- Added run and stop button for thread control
+ * -- Added textboxes for "constant" input
+ * 
+ * Changed cpu to run program in a new thread.
+ */
+
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace eaterIsaSim
+{
+    public partial class Form1 : Form
+    {
+        // Maximum bytes in memory
+        // Default is 16 bytes
+        private uint MAX_BYTES;
+
+        // Program to be executed
+        private uint[] pgm;
+
+        // Ram and registers
+        private Memory mem;
+
+        // Simulated instruction set to run pgm
+        private EaterIsa cpu;
+
+        // Thread to run simulation on
+        Thread sim;
+
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+
+            sim = null;
+        }
+
+        // Called on run button click
+        // 1. Assembles program (exits if error)
+        // 2. Reads "constant" variables from textboxes
+        // 3. Allocates new memory
+        // 4. Creates cpu object with memory reference and constants
+        // 5. Runs program on new thread
+        private void RunProgram_Click(object sender, EventArgs e)
+        {
+            // Do not allow run to be clicked if program running
+            if (sim != null)
+            {
+                return;
+            }
+
+            ProgramOutput.Clear();
+
+            // Maximum number of bytes in memory
+            MAX_BYTES = UInt32.Parse(MaxBytesTB.Text);
+
+            // Assembles program
+            // Displays error message and returns if unsuccessful
+            if (!Assembler.Assemble(ProgramData.Lines, MAX_BYTES))
+            {
+                ProgramOutput.Text = Assembler.GetAsmInfo();
+                ProgramOutput.Text += Environment.NewLine + "Could not assemble program.";
+                return;
+            }
+
+            // Load program from assembler
+            pgm = Assembler.GetProgram();
+
+            ProgramOutput.Text = "Program assembled successfully. " + (MAX_BYTES - Assembler.GetBytesUsed()) + " bytes free." + Environment.NewLine;
+
+            // How many instructions to execute before termination
+            int insnCount = Int32.Parse(MaxInsnsTB.Text);
+
+            // Delay in milliseconds between each instruction
+            int delay = Int32.Parse(InsnDelayTB.Text);
+
+            // Create new memory for the CPU
+            // Initialize it with the created program
+            // and max ram address space
+            mem = new Memory(pgm, MAX_BYTES);
+
+            // Create a new cpu simulation
+            // Initialize it with the created memory
+            cpu = new EaterIsa(ref mem, ref OutputRegTB, insnCount, delay);
+
+            // Create new thread for program
+            // This allows user to still use buttons
+            // and text areas
+            sim = new Thread(start);
+
+            // Execute the program on a new thread
+            sim.Start();
+        }
+
+        // Stop button handler
+        // Terminates the program early
+        private void StopProgram_Click(object sender, EventArgs e)
+        {
+            if (sim != null)
+            {
+                cpu.Terminate();
+                sim = null;
+            }
+        }
+
+        // Called by sim thread to run program
+        private void start()
+        {
+            cpu.Run(ProgramOutput);
+        }
+    }
+}
